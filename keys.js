7022a880d59d7e0fae0a261353297df8775d48c1
Ashley Louitan <jataveius@aol.com>
2018-01-13 09:12:21 -0500

    add file

---
+++ keys.js
@@ -0,0 +1,25 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: "sY40rgbzvuIqsVqqg2BpHs5lf",
+  consumer_secret: "KPNgiLzJu37Zz1dLV1z28Jeh6twNHJMp1FN39wycyouJZhdcrC",
+  access_token_key: "951611458391027713-rhRQgoejgnK0InB3Tba1bVZldmXjS8U",
+  access_token_secret: "Htzfz0PoCON9iJCfpcEVmXsuyWAy9UDpxrFv3u9nJ0Sxa",
+};
+
+exports.spotify ={
+  id: "a4fb2b8559c545b7935bc1afed419e9b",
+  secret: "a4fb2b8559c545b7935bc1afed419e9b",
+};
+
+// exports.twitter = {
+//   consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+//   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+// };
+
+// exports.spotify = {
+//   id: process.env.SPOTIFY_ID,
+//   secret: process.env.SPOTIFY_SECRET
+// };
---
+++ liri.js
@@ -0,0 +1,251 @@
+
+
+require("dotenv").config();
+var keys = require("./keys.js");
+var action = process.argv[2];
+var command = process.argv[2];
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+var Twitter = require("twitter");
+var client = new Twitter(keys.twitter);
+
+
+// Stores the arguments
+var nodeArgs = process.argv;
+
+// Empthy Variable
+var value = "";
+
+// Loops through 
+
+for (var i = 3; i < nodeArgs.length; i++) {
+
+    if (i > 3 && i < nodeArgs.length) {
+
+        value = value + "+" + nodeArgs[i];
+
+    } else {
+
+        value = value + nodeArgs[i];
+    }
+}
+
+
+//Switch Statment for inquirer
+var inquirer = require("inquirer");
+inquirer.prompt([{
+    type: "list",
+    name: "activity",
+    message: "Liri is ready when you are",
+    choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"]
+}]).then(function (response) {
+
+//     var activity = response.activity;
+switch (action) {
+   
+    case "my-tweets":
+        twitter();
+        break;
+
+    case "movie-this":
+        imdb();
+        break;
+
+        case "":
+        spotify();
+        break;
+
+    case "do-what-it-says":
+        dwis();
+        break;
+}
+
+
+
+//TWITTER________________________________________________________
+
+function twitter() {
+    //FS npm 
+    var fs = require("fs");
+
+    //Pulls twitter "keys.js" 
+    var twitterKey = require("./keys.js");
+    // console.log(twitterKey.twitterKeys)
+
+    //Twitter package
+    var Twitter = require("twitter");
+
+
+    var client = new Twitter(twitterKey.twitterKeys);
+
+    //Function that pulls twitter value 
+    var params = {
+        screen_name: value,
+        count: 20
+    };
+    client.get("statuses/user_timeline", params, function (error, tweets, response) {
+        if (!error) {
+
+            console.log("=============================================");
+            console.log("Here are the most recent tweets");
+
+            for (var i = 0; i < tweets.length; i++) {
+
+                console.log("_____________________________________________");
+                console.log("Tweeted on: " + tweets[i].created_at);
+                console.log(tweets[i].text);
+
+            }
+        }
+    });
+
+
+    //SPOTIFY________________________________________________________
+    //FS for npm
+
+    function spotify() {
+        var spotify = require('spotify');
+        var songName = "";
+
+        if (process.argv.length === 3) {
+            songName = "The Sign by Ace of Base";
+        } else {
+
+            for (var i = 3; i < process.argv.length; i++) {
+                if (i > 3) {
+                    songName = songName + " " + process.argv[i];
+                } else {
+                    songName += process.argv[i];
+                }
+            };
+        }
+
+        spotify.search({
+            type: 'track',
+            query: songName,
+            limit: 5
+        }, function (error, data) {
+            if (error) {
+                return console.log("Error occurred:  "+ error);
+            }
+
+            var songResults = data.tracks.items;
+
+            for (let i = 0; i < songResults.length; i++) {
+                console.log("===================================================== \n");
+                var songArtists = [];
+                for (let j = 0; j < songResults[i].artists.length; j++) {
+                    songArtists.push(songResults[i].artists[j].name);
+                    var listOfArtists = songArtists.join(", ");
+                };
+
+                console.log("Song Name: ", songResults[i].name);
+                console.log("Artists: ", listOfArtists);
+                console.log("Preview URL: ", songResults[i].preview_url);
+                console.log("Album Name: ", songResults[i].album.name);
+
+            };
+
+            console.log("============================================================");
+        });
+
+
+        //IMDB________________________________________________________
+
+        function imdb() {
+
+            // Npm package request 
+            var request = require("request");
+
+            // Runs movie request
+            request("http://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=742bef6d",
+                function (error, response, body) {
+
+                    //Succesfull request 200
+                    if (value != false) {
+
+                        // Parse the body data
+                        console.log("======================================================================");
+                        console.log("Title: " + JSON.parse(body).Title);
+                        console.log("Release Year: " + JSON.parse(body).Year);
+                        console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+                        console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+                        console.log("Country where move was produced: " + JSON.parse(body).Country);
+                        console.log("Language of the movie: " + JSON.parse(body).Language);
+                        console.log("Plot of the movie: " + JSON.parse(body).Plot);
+                        console.log("Actors in the movie: " + JSON.parse(body).Actors);
+                    } else {
+
+                        //Mr. Nobody if no user input for movie search     
+                        var request = require("request");
+
+                        request("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&tomatoes=true&r=json",
+                            function (error, response, body) {
+
+                                // Parses through the body data.
+                                console.log("======================================================================");
+                                console.log("Title: " + JSON.parse(body).Title);
+                                console.log("Release Year: " + JSON.parse(body).Year);
+                                console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+                                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+                                console.log("Country where move was produced: " + JSON.parse(body).Country);
+                                console.log("Language of the movie: " + JSON.parse(body).Language);
+                                console.log("Plot of the movie: " + JSON.parse(body).Plot);
+                                console.log("Actors in the movie: " + JSON.parse(body).Actors);
+                            });
+                    }
+                });
+        }
+
+        //do-what-it-says____________________
+
+        function dwis() {
+
+            //FS npm package
+            var fs = require("fs");
+
+            // Reads from the "random.txt" file 
+            fs.readFile("random.txt", "utf8", function (error, data) {
+
+                data = data.split(",");
+
+                var command;
+                var parameter;
+
+                // for (var i = 0; i < data.length; i++) {
+                //     result = data[i];
+                // }
+                if (data.length == 2) {
+                    command = data[0];
+                    parameter = data[1];
+                    // console.log(command);
+                    // console.log(parameter);
+                }
+                // PRINTS THE CONTENTS OF DATA WHICH IS IN RESULT 
+                //console.log(result);
+
+                // if (result != false) {
+                parameter = parameter.replace('"', '');
+                parameter = parameter.replace('"', '');
+                // console.log(parameter);
+
+                switch (command) {
+                    case "my-tweets":
+                        value = parameter;
+                        twitter();
+                        break;
+
+                    case "spotify-this-song":
+                        value = parameter;
+                        spotify();
+                        break;
+
+                    case "movie-this":
+                        value = parameter;
+                        imdb();
+                        break;
+                }
+            })
+        }
+    }
+}
